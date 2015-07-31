e53f9052fb8fde97cd796ecc670001797673d849
Thomas Breloff <tom@breloff.com>
2015-07-31 00:55:20 -0400

    switched constants from dict to type

--- src/utils.jl
+++ src/utils.jl
@@ -1,6 +1,6 @@
 
-const UTAG_MASK_SYMS = [:ubitmask, :signbitmask, :negbitmask, :uboundbitmask, :zerobitmask, :infbitmask, :nanbitmask]
-const UTAG_POS_SYMS = [:ubitpos, :signbitpos, :negbitpos, :uboundbitpos, :zerobitpos, :infbitpos, :nanbitpos]
+# this section is pretty important... I create a type with key constants specific to a certain 
+# parameter set which defines a Unum.  
 
 numbits{T}(::Type{T}) = sizeof(T) * 8
 
@@ -17,41 +17,97 @@ function createmask{UINT<:Unsigned}(::Type{UINT}, left::Int, numones::Int)
   x
 end
 
+# ---------------------------------------------------------------------------------------
+
 # keep a cache for given parameter sets so we don't keep rebuilding the constants
-const constsCache = Dict{Tuple{Int,Int,DataType}, Dict{Symbol, Any}}()
-function getConstants(args...)
-  get!(constsCache, args) do
-    buildConstants(args...)
+# const unumConstCache = Dict{Tuple{Int,Int,DataType}, Dict{Symbol, Any}}()
+
+type UnumInfo{UINT<:Unsigned}
+  base::Int
+  nbits::Int
+  esize::Int
+  fsize::Int
+  utagsize::Int
+  
+  epos::Int
+  fpos::Int
+  ubitpos::Int
+  signbitpos::Int
+  negbitpos::Int
+  uboundbitpos::Int
+  zerobitpos::Int
+  infbitpos::Int
+  nanbitpos::Int
+
+  emask::UINT
+  fmask::UINT
+  ubitmask::UINT
+  signbitmask::UINT
+  negbitmask::UINT
+  uboundbitmask::UINT
+  zerobitmask::UINT
+  infbitmask::UINT
+  nanbitmask::UINT
+
+  # UnumInfo{T}(::Type{T}) = new()
+end
+
+function Base.show{T}(io::IO, info::UnumInfo{T})
+  println("UnumInfo{$T}:")
+  for fn in fieldnames(info)[1:14]
+    println(@sprintf("  %15s %6d", fn, getfield(info, fn)))
+  end
+  for fn in fieldnames(info)[15:end]
+    println(@sprintf("  %15s %s", fn, bits(getfield(info, fn))))
   end
 end
+
+UnumInfo{T}(::Type{T}) = UnumInfo{T}(zeros(Int, 14)..., zeros(T, 9)...)
+
+
+# keep a cache for given parameter sets so we don't keep rebuilding the constants
+const unumConstCache = Dict{Tuple{Int,Int,DataType}, UnumInfo}()
 
 # expect this to be called from a generated function, so we're being passed type params
-function buildConstants(EBASE::Int, ESZ::Int, UINT::DataType)
-  d = Dict{Symbol, Any}()
+function unumConstants(EBASE::Int, ESZ::Int, UINT::DataType)
+  get!(unumConstCache, (EBASE, ESZ, UINT)) do
+    info = UnumInfo(UINT)
+    info.base = EBASE
+    info.nbits = numbits(UINT)
+    info.esize = ESZ
+    info.utagsize = 7
+    info.fsize = info.nbits - info.utagsize - info.esize
+    @assert info.fsize > 0
 
-  # first some basic calcs
-  d[:nbits] = numbits(UINT)
-  d[:utagsize] = 7
-  d[:esize] = ESZ
-  d[:fsize] = d[:nbits] - d[:utagsize] - ESZ
-  @assert d[:fsize] > 0
+    info.epos = info.nbits
+    info.fpos = info.nbits - info.esize
+    info.ubitpos = 1
+    info.signbitpos = 2
+    info.negbitpos = 3
+    info.uboundbitpos = 4
+    info.zerobitpos = 5
+    info.infbitpos = 6
+    info.nanbitpos = 7
 
-  # leftmost positions
-  d[:epos] = d[:nbits]
-  d[:fpos] = d[:nbits] - d[:esize]
-  for (i,s) in enumerate(UTAG_POS_SYMS)
-    d[s] = i
+    info.emask = createmask(UINT, info.epos, info.esize)
+    info.fmask = createmask(UINT, info.fpos, info.fsize)
+    info.ubitmask = createmask(UINT, info.ubitpos, 1)
+    info.signbitmask = createmask(UINT, info.signbitpos, 1)
+    info.negbitmask = createmask(UINT, info.negbitpos, 1)
+    info.uboundbitmask = createmask(UINT, info.uboundbitpos, 1)
+    info.zerobitmask = createmask(UINT, info.zerobitpos, 1)
+    info.infbitmask = createmask(UINT, info.infbitpos, 1)
+    info.nanbitmask = createmask(UINT, info.nanbitpos, 1)
+
+    info
   end
-
-  # masks
-  d[:emask] = createmask(UINT, d[:epos], d[:esize])
-  d[:fmask] = createmask(UINT, d[:fpos], d[:fsize])
-  for (i, s) in enumerate(UTAG_MASK_SYMS)
-    d[s] = createmask(UINT, d[UTAG_POS_SYMS[i]], 1)
-  end
-
-  d
 end
+
+# ---------------------------------------------------------------------------------------
+
+type FloatInfo{F<:FloatingPoint}
+end
+
 
 # ---------------------------------------------------------------------------------------
 
@@ -59,11 +115,11 @@ end
 # this is the intended method of creating functions... get the constants from the parameters, 
 # then build a specialized (generated) function for this combination
 @generated function Base.show{EBASE,ESZ,UINT}(io::IO, u::FixedUnum{EBASE,ESZ,UINT})
-  d = getConstants(EBASE, ESZ, UINT)
+  c = unumConstants(EBASE, ESZ, UINT)
 
-  efsz = ESZ + d[:fsize]
+  efsz = ESZ + c.fsize
   layout = [("exp", 1 : ESZ),
-            ("frac", (1 : d[:fsize]) + ESZ),
+            ("frac", (1 : c.fsize) + ESZ),
             ("NaN?", ESZ + 1),
             ("Inf?", ESZ + 2),
             ("zero?", ESZ + 3),
